9c78574c90659b1f8801d402b6be5e2ab790be68
Guillem Puche <3519924+guillempuche@users.noreply.github.com>
2022-04-23 16:36:41 +0200

    feat: improve the transaction entities

--- lib/entities/transaction_receive.ts
+++ lib/entities/transaction_receive.ts
@@ -1,13 +1,15 @@
-import { Ok, Result } from 'ts-results';
+import { Err, Ok, Result } from 'ts-results';
 import { Entity } from '../shared/entity';
+import { Guard } from '../shared/guard';
+import { UniqueEntityId } from '../shared/unique_entity_id';
 
-interface TransactionReceiveProps {
+export interface TransactionReceiveProps {
 	address: string;
 	amount: number;
 }
 
 export class TransactionReceive extends Entity<TransactionReceiveProps> {
-	get id(): string {
+	get id(): UniqueEntityId {
 		return this._id;
 	}
 
@@ -15,16 +17,19 @@ export class TransactionReceive extends Entity<TransactionReceiveProps> {
 		return this.props.address;
 	}
 
-	private constructor(props: TransactionReceiveProps, id?: string) {
+	private constructor(props: TransactionReceiveProps, id?: UniqueEntityId) {
 		super(props, id);
 	}
 
 	public static create(
 		props: TransactionReceiveProps,
-		id?: string
-	): Result<TransactionReceive, any> {
-		// TODO: add value objects (for validating the fields email and password).
-		// If there's an error, return Err()
+		id?: UniqueEntityId
+	): Result<TransactionReceive, string> {
+		const notNegativeGuard = Guard.greaterThan(0, props.amount);
+
+		if (!notNegativeGuard.succeeded) {
+			Err(notNegativeGuard.succeeded!);
+		}
 
 		return Ok(new TransactionReceive(props, id));
 	}
--- lib/entities/transaction_send.ts
+++ lib/entities/transaction_send.ts
@@ -1,16 +1,18 @@
-import { Ok, Result } from 'ts-results';
+import { Err, Ok, Result } from 'ts-results';
 import { Entity } from '../shared/entity';
-import { Currency } from './types';
+import { Guard } from '../shared/guard';
+import { UniqueEntityId } from '../shared/unique_entity_id';
+import { Token } from './tokens';
 
-interface TransactionSendProps {
+export interface TransactionSendProps {
 	address: string;
-	currency: Currency;
+	token: Token;
 	amount: number;
 	fee: number;
 }
 
 export class TransactionSend extends Entity<TransactionSendProps> {
-	get id(): string {
+	get id(): UniqueEntityId {
 		return this._id;
 	}
 
@@ -18,8 +20,8 @@ export class TransactionSend extends Entity<TransactionSendProps> {
 		return this.props.address;
 	}
 
-	get currency(): Currency {
-		return this.props.currency;
+	get token(): Token {
+		return this.props.token;
 	}
 
 	get amount(): number {
@@ -30,16 +32,19 @@ export class TransactionSend extends Entity<TransactionSendProps> {
 		return this.props.fee;
 	}
 
-	private constructor(props: TransactionSendProps, id?: string) {
+	private constructor(props: TransactionSendProps, id?: UniqueEntityId) {
 		super(props, id);
 	}
 
 	public static create(
 		props: TransactionSendProps,
-		id?: string
-	): Result<TransactionSend, any> {
-		// TODO: add value objects (for validating the fields email and password).
-		// If there's an error, return Err()
+		id?: UniqueEntityId
+	): Result<TransactionSend, string> {
+		const notNegativeGuard = Guard.greaterThan(0, props.amount);
+
+		if (!notNegativeGuard.succeeded) {
+			Err(notNegativeGuard.succeeded!);
+		}
 
 		return Ok(new TransactionSend(props, id));
 	}
