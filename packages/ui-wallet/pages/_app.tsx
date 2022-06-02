212993488af20b98b06a1c24de1b06f049891260
Guillem Puche <3519924+guillempuche@users.noreply.github.com>
2022-06-02 18:19:50 +0200

    feat(app): add the state management tool `Recoil`

--- packages/ui-wallet/pages/_app.tsx
+++ packages/ui-wallet/pages/_app.tsx
@@ -1,11 +1,12 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { Layout, NavigationBar } from '@skiawallet/ui-components';
-import '../styles/styles.css';
+import { RecoilRoot } from 'recoil';
+
+import '../styles/global.css';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <>
+    <RecoilRoot>
       <Head>
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         <meta
@@ -13,9 +14,9 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
           content="width=device-width, initial-scale=1.0"
         ></meta>
       </Head>
-      <Layout navigationBar={NavigationBar()}>
-        <Component {...pageProps} />
-      </Layout>
-    </>
+      {/* <Layout navigationBar={NavigationBar()}> */}
+      <Component {...pageProps} />
+      {/* </Layout> */}
+    </RecoilRoot>
   );
 }
