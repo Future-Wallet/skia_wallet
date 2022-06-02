bd5e21cc456678773b30ecf398aa4ff93375bd3a
Guillem Puche <3519924+guillempuche@users.noreply.github.com>
2022-06-02 18:23:46 +0200

    wip(ui-wallet): editing the page Getting Started

--- packages/ui-wallet/pages/getting-started.tsx
+++ packages/ui-wallet/pages/getting-started.tsx
@@ -1,16 +1,11 @@
-import { AccountRepositoryPortis } from '@skiawallet/repositories';
-import { Button, Center, Stack } from '@skiawallet/ui-components';
+import { Center, Stack } from '@skiawallet/ui-components';
 
 export default function GettingStarted() {
-  function openPortis() {
-    // AccountRepositoryPortis;
-  }
   return (
     <Stack>
       <Center>
         <h1>Getting started with the wallet</h1>
       </Center>
-      <Button onClick={openPortis}>Signin wallet</Button>
     </Stack>
   );
 }
