2305919df754747d514f75aad026a228ce7e576c
Guillem Puche <3519924+guillempuche@users.noreply.github.com>
2022-07-10 14:50:15 +0200

    fix(prototype): fix build config of Next

--- packages/prototype/next.config.js
+++ packages/prototype/next.config.js
@@ -1,6 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
-
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
@@ -14,4 +12,3 @@ const nextConfig = {
 };
 
 module.exports = withNx(nextConfig);
-
