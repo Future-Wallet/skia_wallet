7361d6cb0d4810b26ff6a8381e4e3836cb532b84
Guillem Puche <3519924+guillempuche@users.noreply.github.com>
2022-08-02 19:44:54 +0200

    improv(ui-wallet,settings): improve settings functionality and UI

--- packages/ui-wallet/src/components/settings.tsx
+++ packages/ui-wallet/src/components/settings.tsx
@@ -1,19 +1,42 @@
 import { useRecoilState } from 'recoil';
+
 import { stateUserWallet } from '../state/wallet';
+import { copyValueToClipboard } from '../utils/miscellaneous';
 import Button from './atomic/button';
 
 export default function Settings(): JSX.Element {
-  const [userWallet] = useRecoilState(stateUserWallet);
+  const [userWallet, setUserWallet] = useRecoilState(stateUserWallet);
+
   return (
-    <>
+    <div>
       <p>Your private phrase</p>
       <textarea
         rows={3}
         readOnly={true}
-        className="mt-2 mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        className="my-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
         value={userWallet?.props.mnemonicPhrase}
       />
-      <Button>Delete all your data from this browser</Button>
-    </>
+      <Button
+        onClick={() => {
+          if (userWallet !== null) {
+            copyValueToClipboard(userWallet.props.mnemonicPhrase);
+          }
+        }}
+      >
+        Copy address
+      </Button>
+      <p className="mt-8 mb-2">
+        Delete all your data from this browser (it won't delete any data on the
+        blockchain)
+      </p>
+      <Button
+        onClick={() => {
+          // Delete data of the `localStorage`
+          setUserWallet(null);
+        }}
+      >
+        Delete data
+      </Button>
+    </div>
   );
 }
