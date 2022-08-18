b7cbf92e99ca5d982859a4ab7b98da66eddd8afa
Guillem Puche <3519924+guillempuche@users.noreply.github.com>
2022-08-18 22:24:10 +0200

    feat(ui-common, logger): add a logger for debugging on the console

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/entity';
 export * from './lib/guard';
+export * from './lib/logger';
 export * from './lib/unique_entity_id';
 export * from './lib/value_object';
---
+++ packages/common/src/lib/logger.ts
@@ -0,0 +1,56 @@
+import { createLogger, format, transports } from 'winston';
+
+interface LogMessage {
+  message: string;
+}
+
+type Logger = {
+  debug(logMessage: LogMessage): void;
+  debug(message: string): void;
+  info(logMessage: LogMessage): void;
+};
+
+/**
+ * The logger prints the information on the console.
+ *
+ * It only works when it's not in production (`process.env.ENV` isn't `production`).
+ *
+ */
+export const logger: Logger = {
+  debug(data): void {
+    if (
+      process.env['NODE_ENV'] !== 'production' &&
+      process.env['NX_ENABLE_LOGGER'] == 'true'
+    ) {
+      const logger = createLogger({
+        level: 'debug',
+        format: format.combine(
+          format.printf(({ level, message, timestamp }) => {
+            return `${level} ${timestamp}: ${message}`;
+          })
+        ),
+        transports: new transports.Console(),
+      });
+
+      logger.debug({ message: typeof data == 'string' ? data : data.message });
+    }
+  },
+  info(data): void {
+    if (
+      process.env['NODE_ENV'] !== 'production' &&
+      process.env['NX_ENABLE_LOGGER'] == 'true'
+    ) {
+      const logger = createLogger({
+        level: 'info',
+        format: format.combine(
+          format.printf(({ level, message, timestamp }) => {
+            return `${level} ${timestamp}: ${message}`;
+          })
+        ),
+        transports: new transports.Console(),
+      });
+
+      logger.info({ message: typeof data == 'string' ? data : data.message });
+    }
+  },
+};
