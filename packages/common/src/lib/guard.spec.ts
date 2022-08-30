03d9b63f26af95da7b24b2bef843889c70ee5020
Guillem Puche <3519924+guillempuche@users.noreply.github.com>
2022-08-30 21:31:30 +0200

    fix(common, guard): fix `isAnEvmAddress`, comment unused methods

---
+++ packages/common/src/lib/guard.spec.ts
@@ -0,0 +1,130 @@
+import { Error } from './error';
+import {
+  ErrorAddressDoesntStartWith0x,
+  ErrorAddressLessThan42Characters,
+  ErrorAddressGreaterThan42Characters,
+  ErrorAddressNotHexadecimal,
+  ErrorAddressInvalidChecksum,
+} from './error_address';
+import { Guard, GuardManyResults } from './guard';
+
+describe('class Guard', () => {
+  describe('isAnEvmAddress()', () => {
+    let result: GuardManyResults<string>;
+    let address;
+    let typeOfErrors: string[];
+
+    it(`approves '0xa9d48F598d7A573d840F318f40d77D5a50908c18'`, () => {
+      address = '0xa9d48F598d7A573d840F318f40d77D5a50908c18';
+      result = Guard.isAnEvmAddress(address);
+
+      expect(result.ok).toBeTruthy();
+    });
+
+    it(`disapproves 'random_characters'`, () => {
+      address = 'random_characters';
+      result = Guard.isAnEvmAddress(address);
+      typeOfErrors = (result.val as Error<string>[]).map((err) =>
+        err.getClassName()
+      );
+
+      expect(result.err).toBeTruthy();
+      expect(expect.arrayContaining(typeOfErrors)).toEqual([
+        ErrorAddressDoesntStartWith0x.name,
+        ErrorAddressLessThan42Characters.name,
+        ErrorAddressNotHexadecimal.name,
+      ]);
+    });
+
+    it(`disapproves '1234'`, () => {
+      address = '1234';
+      result = Guard.isAnEvmAddress(address);
+      typeOfErrors = (result.val as Error<string>[]).map((err) =>
+        err.getClassName()
+      );
+
+      expect(result.err).toBeTruthy;
+      expect(expect.arrayContaining(typeOfErrors)).toEqual([
+        ErrorAddressDoesntStartWith0x.name,
+        ErrorAddressLessThan42Characters.name,
+        ErrorAddressNotHexadecimal.name,
+      ]);
+    });
+
+    it(`disapproves '123456789012345678901234567890123456789012'`, () => {
+      address = '123456789012345678901234567890123456789012';
+      result = Guard.isAnEvmAddress(address);
+      typeOfErrors = (result.val as Error<string>[]).map((err) =>
+        err.getClassName()
+      );
+
+      expect(result.err).toBeTruthy;
+      expect(expect.arrayContaining(typeOfErrors)).toEqual([
+        ErrorAddressDoesntStartWith0x.name,
+        ErrorAddressGreaterThan42Characters.name,
+        ErrorAddressNotHexadecimal.name,
+      ]);
+    });
+
+    it(`disapproves '0x1234'`, () => {
+      address = '0x1234';
+      result = Guard.isAnEvmAddress(address);
+      typeOfErrors = (result.val as Error<string>[]).map((err) =>
+        err.getClassName()
+      );
+
+      expect(result.err).toBeTruthy;
+      expect(expect.arrayContaining(typeOfErrors)).toEqual([
+        ErrorAddressLessThan42Characters.name,
+        ErrorAddressNotHexadecimal.name,
+      ]);
+    });
+
+    it(`disapproves '0x12345678901234567890123456789012345678901234567890'`, () => {
+      address = '0x12345678901234567890123456789012345678901234567890';
+      result = Guard.isAnEvmAddress(address);
+      typeOfErrors = (result.val as Error<string>[]).map((err) =>
+        err.getClassName()
+      );
+
+      expect(result.err).toBeTruthy;
+      expect(expect.arrayContaining(typeOfErrors)).toEqual([
+        ErrorAddressGreaterThan42Characters.name,
+        ErrorAddressLessThan42Characters.name,
+      ]);
+    });
+
+    it(`disapproves '0xzzzz567890123456789012345678901234567890'`, () => {
+      address = '0xzzzz567890123456789012345678901234567890';
+      result = Guard.isAnEvmAddress(address);
+      typeOfErrors = (result.val as Error<string>[]).map((err) =>
+        err.getClassName()
+      );
+
+      expect(result.err).toBeTruthy;
+      expect(expect.arrayContaining(typeOfErrors)).toEqual([
+        ErrorAddressNotHexadecimal.name,
+      ]);
+    });
+
+    it(`approves a right checksum address '0xa54d3c09E34aC96807c1CC397404bF2B98DC4eFb'`, () => {
+      address = '0xa54d3c09E34aC96807c1CC397404bF2B98DC4eFb';
+      result = Guard.isAnEvmAddress(address);
+
+      expect(result.ok).toBeTruthy;
+    });
+
+    it(`disapproves a wrong checksum address '0xa54D3c09E34aC96807c1CC397404bF2B98DC4eFb'`, () => {
+      address = '0xa54D3c09E34aC96807c1CC397404bF2B98DC4eFb';
+      result = Guard.isAnEvmAddress(address);
+      typeOfErrors = (result.val as Error<string>[]).map((err) =>
+        err.getClassName()
+      );
+
+      expect(result.err).toBeTruthy;
+      expect(expect.arrayContaining(typeOfErrors)).toEqual([
+        ErrorAddressInvalidChecksum.name,
+      ]);
+    });
+  });
+});
--- packages/common/src/lib/guard.ts
+++ packages/common/src/lib/guard.ts
@@ -1,114 +1,193 @@
-export interface IGuardResult {
-  succeeded: boolean;
-  message?: string;
-}
+import { arrayify, keccak256 } from 'ethers/lib/utils';
+import { Err, Ok, Result } from 'ts-results';
 
-export interface IGuardArgument {
-  argument: any;
-  argumentName: string;
-}
+import { Error } from './error';
+import {
+  ErrorAddressDoesntStartWith0x,
+  ErrorAddressGreaterThan42Characters,
+  ErrorAddressInvalidChecksum,
+  ErrorAddressLessThan42Characters,
+  ErrorAddressNotHexadecimal,
+} from './error_address';
 
-export type GuardArgumentCollection = IGuardArgument[];
+export type GuardResult<T> = Result<GuardResponse, Error<T>>;
+export type GuardManyResults<T> = Result<GuardResponse, Error<T>[]>;
 
-// TODO: create test that always return message when the validation is incorrect.
-// TODO: translate error messages.
+export type GuardResponse = string | undefined;
+
 /**
- * Multiple methods to verify data.
+ * Multiple utilities to verify if the data is correct.
  */
 export class Guard {
-  public static greaterThan(
-    minValue: number,
-    actualValue: number
-  ): IGuardResult {
-    return actualValue > minValue
-      ? { succeeded: true }
-      : {
-          succeeded: false,
-          message: `Number given {${actualValue}} is not greater than {${minValue}}`,
-        };
-  }
-
-  public static againstAtLeast(numChars: number, text: string): IGuardResult {
-    return text.length >= numChars
-      ? { succeeded: true }
-      : {
-          succeeded: false,
-          message: `Text is not at least ${numChars} chars.`,
-        };
-  }
-
-  public static againstAtMost(numChars: number, text: string): IGuardResult {
-    return text.length <= numChars
-      ? { succeeded: true }
-      : {
-          succeeded: false,
-          message: `Text is greater than ${numChars} chars.`,
-        };
-  }
-
-  public static againstNullOrUndefined(
-    argument: any,
-    argumentName: string
-  ): IGuardResult {
-    if (argument === null || argument === undefined) {
-      return {
-        succeeded: false,
-        message: `${argumentName} is null or undefined`,
-      };
-    } else {
-      return { succeeded: true };
-    }
-  }
-
-  public static againstNullOrUndefinedBulk(
-    args: GuardArgumentCollection
-  ): IGuardResult {
-    for (let arg of args) {
-      const result = this.againstNullOrUndefined(
-        arg.argument,
-        arg.argumentName
-      );
-      if (!result.succeeded) return result;
-    }
-    return { succeeded: true };
-  }
-
   /**
+   * Check if is an EVM address (including the checksum address)
    *
-   * IMPORTANT: Error message
+   * Code copied from [EtherJS](https://github.com/ethers-io/ethers.js/blob/01b5badbb616b29fd8b69ef7c3cc3833062da3d7/packages/address/src.ts/index.ts#L77).
+   *
    * @param address
    * @returns
    */
-  public static isAnEVMAddress(address: string): IGuardResult {
-    const section_one = address.slice(0, 1);
-    const section_two = address.slice(2);
-    let errorMessages: string[] = [`Address ${address}.`];
+  public static isAnEvmAddress(address: string): GuardManyResults<string> {
+    const errors: Error<string>[] = [];
 
-    if (section_one != '0x') {
-      errorMessages.push(`Address must start with 0x.`);
+    // Missing the 0x prefix
+    if (address.slice(0, 2) != '0x') {
+      errors.push(new ErrorAddressDoesntStartWith0x(address));
     }
-    if (section_two.length != 40) {
-      errorMessages.push(
-        `Address must have 40 characters instead of ${section_two.length}.`
-      );
+
+    if (!isHexString(address)) {
+      errors.push(new ErrorAddressNotHexadecimal(address));
+    }
+
+    if (address.length < 42) {
+      errors.push(new ErrorAddressLessThan42Characters(address));
+    } else if (address.length > 42) {
+      errors.push(new ErrorAddressGreaterThan42Characters(address));
+    } else {
+      // // Missing the 0x prefix
+      // if (address.substring(0, 2) !== '0x') {
+      //   address = '0x' + address;
+      // }
+
+      const result = getChecksumAddress(address);
+
+      if (typeof result !== 'string') {
+        errors.push(result);
+      } else {
+        // It's a checksummed address with a bad checksum
+        if (
+          address.match(/([A-F].*[a-f])|([a-f].*[A-F])/) &&
+          result !== address
+        ) {
+          errors.push(new ErrorAddressInvalidChecksum(address));
+        }
+      }
     }
 
     // If there aren't error messages, it means the address is correct.
-    if (errorMessages.length == 1) {
-      return { succeeded: true };
+    if (errors.length == 0) {
+      return Ok(address);
     }
 
-    return {
-      succeeded: false,
-      message: errorMessages.join(' '),
-    };
+    return Err(errors);
   }
+
+  // public static againstNullOrUndefined(
+  //   argument: any,
+  //   argumentName: string
+  // ): IGuardResult<any> {
+  //   if (argument === null || argument === undefined) {
+  //     return {
+  //       succeeded: false,
+  //       error: `${argumentName} is null or undefined`,
+  //     };
+  //   } else {
+  //     return { succeeded: true };
+  //   }
+  // }
+
+  // public static againstNullOrUndefinedBulk(
+  //   args: GuardArgumentCollection
+  // ): IGuardResult<GuardArgumentCollection> {
+  //   for (const arg of args) {
+  //     const result = Guard.againstNullOrUndefined(
+  //       arg.argument,
+  //       arg.argumentName
+  //     );
+  //     if (!result.succeeded) return result;
+  //   }
+  //   return { succeeded: true };
+  // }
+
+  // public static greaterThan(
+  //   minValue: number,
+  //   actualValue: number
+  // ): IGuardResult {
+  //   return actualValue > minValue
+  //     ? { succeeded: true }
+  //     : {
+  //         succeeded: false,
+  //         message: `Number given {${actualValue}} is not greater than {${minValue}}`,
+  //       };
+  // }
+
+  // public static againstAtLeast(numChars: number, text: string): IGuardResult {
+  //   return text.length >= numChars
+  //     ? { succeeded: true }
+  //     : {
+  //         succeeded: false,
+  //         message: `Text is not at least ${numChars} chars.`,
+  //       };
+  // }
+
+  // public static againstAtMost(numChars: number, text: string): IGuardResult {
+  //   return text.length <= numChars
+  //     ? { succeeded: true }
+  //     : {
+  //         succeeded: false,
+  //         message: `Text is greater than ${numChars} chars.`,
+  //       };
+  // }
 
   // public static combine(guardResults: IGuardResult[]): IGuardResult {
   // 	for (let result of guardResults) {
   // 		if (result.succeeded === false) return result;
   // 	}
-
   // 	return { succeeded: true };
   // }
+}
+
+/**
+ * Get the checksum of the address.
+ *
+ * Copied from [EtherJS](https://github.com/ethers-io/ethers.js/blob/01b5badbb616b29fd8b69ef7c3cc3833062da3d7/packages/address/src.ts/index.ts#L12)
+ *
+ * @param address
+ * @returns
+ */
+function getChecksumAddress(address: string): string | Error<string> {
+  if (!isHexString(address, 20)) {
+    return new ErrorAddressNotHexadecimal(address);
+  }
+
+  address = address.toLowerCase();
+
+  const chars = address.substring(2).split('');
+
+  const expanded = new Uint8Array(40);
+  for (let i = 0; i < 40; i++) {
+    expanded[i] = chars[i].charCodeAt(0);
+  }
+
+  const hashed = arrayify(keccak256(expanded));
+
+  for (let i = 0; i < 40; i += 2) {
+    if (hashed[i >> 1] >> 4 >= 8) {
+      chars[i] = chars[i].toUpperCase();
+    }
+    if ((hashed[i >> 1] & 0x0f) >= 8) {
+      chars[i + 1] = chars[i + 1].toUpperCase();
+    }
+  }
+
+  return '0x' + chars.join('');
+}
+
+/**
+ * Check if the value is an hexadecimal.
+ *
+ * Code copied from [EtherJS](https://github.com/ethers-io/ethers.js/blob/01b5badbb616b29fd8b69ef7c3cc3833062da3d7/packages/bytes/src.ts/index.ts#L188).
+ *
+ * @param value
+ * @param length
+ * @returns
+ */
+function isHexString(value: string, length?: number): boolean {
+  if (!value.match(/^0x[0-9A-Fa-f]*$/)) {
+    return false;
+  }
+  if (length && value.length !== 2 + 2 * length) {
+    return false;
+  }
+  return true;
 }
