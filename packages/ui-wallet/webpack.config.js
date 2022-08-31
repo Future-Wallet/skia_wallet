b760f0a2641d9e631eff3466e6316d74b6d891a5
Guillem Puche <3519924+guillempuche@users.noreply.github.com>
2022-08-31 21:53:06 +0200

    fix(ui-wallet, polyfills): disable dependencies for the logger

--- packages/ui-wallet/webpack.config.js
+++ packages/ui-wallet/webpack.config.js
@@ -2,9 +2,13 @@ const { merge } = require('webpack-merge');
 const webpack = require('webpack');
 
 /**
- * Combine default Webpack configuration from Nx with a custom configuration.
+ * Dependencies used on core layers (eg. Winston, bip39...) needs the
+ * [polyfill](https://developer.mozilla.org/en-US/docs/Glossary/Polyfill),
+ * a piece of code to provide modern functionality on older browsers that
+ * do not natively support it.
  *
- * More here: https://nx.dev/guides/customize-webpack
+ * Combine default Webpack configuration from Nx with a custom
+ * configuration. More here: https://nx.dev/recipe/customize-webpack
  */
 module.exports = (config, context) => {
   return merge(config, {
@@ -16,8 +20,26 @@ module.exports = (config, context) => {
     ],
     resolve: {
       fallback: {
+        // Used by bip39
         stream: require.resolve('stream-browserify'),
         buffer: require.resolve('buffer'),
+
+        // Used by dependency Winston
+        os: false,
+        util: false,
+        path: false,
+        zlib: false,
+        http: false,
+        https: false,
+        assert: false,
+        // os: require.resolve('os-browserify/browser'),
+        // util: require.resolve('util/'),
+        // path: require.resolve('path-browserify'),
+        // zlib: require.resolve('browserify-zlib'),
+        // http: require.resolve('stream-http'),
+        // https: require.resolve('https-browserify'),
+        // assert: require.resolve('assert/'),
+        fs: false,
       },
     },
   });
