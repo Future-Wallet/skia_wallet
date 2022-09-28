5ade2dfc9564cdb9f7fb6e01de8ce9116dd06b98
Guillem Puche <3519924+guillempuche@users.noreply.github.com>
2022-09-28 21:26:53 +0200

    build(ci): add CI/CD using Husky and Github Workflows and Actions

---
+++ scripts/.commit_config.js
@@ -0,0 +1,70 @@
+/**
+ * Restrictions for the commit message following the commits  guide
+ * https://github.com/Future-Wallet/skia-wallet/wiki/Commits
+ * 
+ * Inspired by Nx's repository https://github.com/nrwl/nx/commits/master/.cz-config.js
+ */
+module.exports = {
+  types: [
+    { value: 'feat', name: 'feat:    A new feature' },
+    { value: 'fix', name: 'fix:    A bug fix' },
+    { value: 'docs', name: 'docs:    Documentation only changes' },
+    {
+      value: 'refactor',
+      name: 'refactor:    Restructure the code, change the internal behavior of method. Neither fixes a bug nor adds a feature',
+    },
+    {
+      value: 'improv',
+      name: "improv:    An improvement, neither a bug fix nor a refactoring",
+    },
+    {
+      value: 'chore',
+      name: "chore:    Other changes that don't modify the packages or test files",
+    },
+    {
+      value: 'build',
+      name: "build:    Debug tasks, edit git configuration, modify the build process, edit scripts, etc",
+    },
+  ],
+
+  scopes: [
+    { name: 'common', description: 'Anything related to the package Common' },
+    { name: 'entities', description: 'Anything related to the package Entities' },
+    { name: 'repositories', description: 'Anything related to the package Repositories' },
+    { name: 'ui-components', description: 'Anything related to the package containing the frontend components' },
+    { name: 'ui-wallet', description: 'Anything related to the package with wallet website' },
+    { name: '*', description: 'Multiple packages' },
+  ],
+
+  // Add tickets references (Github's issues or other task management tickets id)
+  allowTicketNumber: true,
+  isTicketNumberRequired: false,
+  ticketNumberPrefix: 'TICKET-',
+  ticketNumberRegExp: '\\d{1,5}',
+
+  // override the messages, defaults are as follows
+  messages: {
+    type: "Select the type of change that you're committing:",
+    scope: '\nDenote the SCOPE of this change (optional):',
+    // used if allowCustomScopes is true
+    customScope: 'Denote the SCOPE of this change:',
+    subject:
+      'Write a SHORT, IMPERATIVE (lowercase) description of the change:\n',
+    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+    breaking: 'List any BREAKING CHANGES (optional):\n',
+    footer:
+      'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
+    confirmCommit: 'Are you sure you want to proceed with the commit above?',
+  },
+
+  allowCustomScopes: false,
+  allowBreakingChanges: ['feat', 'fix'],
+  // skip any questions you want
+  skipQuestions: ['ticketNumber'],
+
+  // limit subject length
+  subjectLimit: 100,
+  // breaklineChar: '|', // It is supported for fields body and footer.
+  // footerPrefix : 'ISSUES CLOSED:'
+  // askForBreakingChangeFirst : true, // default is false
+};
